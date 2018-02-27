ed79926beb288449ea6b99e30d8cd32b429dcbf3
servicetraining <marvin.castelino@gmail.com>
2018-02-27 23:36:26 +0400

    testing2

---
+++ script.js
@@ -0,0 +1,66 @@
+function initMap() {
+	
+	var broadway = {
+		info: '<strong>Chipotle on Broadway</strong><br>\
+					5224 N Broadway St<br> Chicago, IL 60640<br>\
+					<a href="https://goo.gl/maps/jKNEDz4SyyH2">Get Directions</a>\
+					<video href="https://www.youtube.com/watch?v=6FZH652qYkA">Get Directions</video>',
+		lat: 41.976816,
+		long: -87.659916
+	};
+
+	var belmont = {
+		info: '<strong>Chipotle on Belmont</strong><br>\
+					1025 W Belmont Ave<br> Chicago, IL 60657<br>\
+					<a href="https://goo.gl/maps/PHfsWTvgKa92">Get Directions</a>',
+		lat: 38.1288342,
+		long: -22.238334
+	};
+
+	var sheridan = {
+		info: '<strong>Chipotle on Sheridan</strong><br>\r\
+					6600 N Sheridan Rd<br> Chicago, IL 60626<br>\
+					<a href="https://goo.gl/maps/QGUrqZPsYp92">Get Directions</a>',
+		lat: 42.002707,
+		long: -87.661236
+	};
+
+	var locations = [
+      [broadway.info, broadway.lat, broadway.long, 0],
+      [belmont.info, belmont.lat, belmont.long, 1],
+      [sheridan.info, sheridan.lat, sheridan.long, 2],
+    ];
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 2,
+		center: new google.maps.LatLng(38.1288342,22.238334),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+
+	var infowindow = new google.maps.InfoWindow({});
+
+	var marker, i;
+
+	for (i = 0; i < locations.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map: map
+			
+		});
+
+		google.maps.event.addListener(marker, 'click', (function (marker, i) {
+			return function () {
+				infowindow.setContent(locations[i][0]);
+				infowindow.open(map, marker);
+			}
+		})(marker, i));
+	}
+}
+
+ function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+      }
